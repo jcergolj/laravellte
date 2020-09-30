6b1f47b4b5eff2d5654d2c4e7ce6e8917a5edb53
Janez Cergolj <me@jcergolj.me.uk>
2020-09-30 12:16:01 +0200

    add show/hide option to password input field

--- resources/views/components/inputs/password.blade.php
+++ resources/views/components/inputs/password.blade.php
@@ -1,7 +1,8 @@
-@props(['key' => 'password'])
+@props(['key' => 'password', 'showHidePasswordIcon' => false])
 
-<div class="input-group mb-3">
+<div class="input-group mb-3" x-data="{ showPassword: false }">
     <input
+        x-ref="{{ $key }}"
         wire:model.defer="{{ $key }}"
         type="password"
         name="{{ $key }}"
@@ -11,6 +12,29 @@
     >
 
     <x-inputs.fa fontAwesome="fa-lock" />
+
+    @if ($showHidePasswordIcon)
+        <div
+            x-on:click="
+                showPassword = !showPassword;
+                if (showPassword) {
+                    $refs.{{ $key }}.type = 'text';
+                    $refs.icon.classList.add('fa-eye-slash');
+                    $refs.icon.classList.remove('fa-eye');
+                }
+                else {
+                    $refs.{{ $key }}.type = 'password';
+                    $refs.icon.classList.add('fa-eye');
+                    $refs.icon.classList.remove('fa-eye-slash');
+                }
+            "
+            class="input-group-append"
+        >
+            <div class="input-group-text">
+                <span x-ref="icon" class="fas fa-eye"></span>
+            </div>
+        </div>
+    @endif
 
     <x-inputs.error field="{{ $key }}" />
 </div>
